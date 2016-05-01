1768c52ae48121331c5591407526191870fce551
Daniel Katz <Daniel@Daniels-MacBook-Pro.local>
2016-05-01 15:09:17 -0400

    first commit

---
+++ materialNavBar.h
@@ -0,0 +1,13 @@
+//
+//  materialNavBar.h
+//  Material NavBar and StatusBar
+//
+//  Created by Daniel Katz on 5/1/16.
+//  Copyright © 2016 Stratton Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface materialNavBar : UINavigationBar
+
+@end
---
+++ materialNavBar.m
@@ -0,0 +1,21 @@
+//
+//  materialNavBar.m
+//  Material NavBar and StatusBar
+//
+//  Created by Daniel Katz on 5/1/16.
+//  Copyright © 2016 Stratton Design. All rights reserved.
+//
+
+#import "materialNavBar.h"
+
+@implementation materialNavBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
